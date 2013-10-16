7d08fc6d3fb6fa07f2628581ac98702b8d0651bd
kumarsaurabh20 <kumarsaurabh20@gmail.com>
2013-10-17 01:15:28 +0200

    update

--- init.rb
+++ init.rb
@@ -1,4 +1,5 @@
 #A ruby interactive program to convert scaffold fasta formatted file in to .agp specification file
+#under construction
 
 APP_ROOT = File.dirname(__FILE__)
 $:.unshift(File.join(APP_ROOT, 'lib'))
