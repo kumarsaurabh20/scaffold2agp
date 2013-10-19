91c4df545678e5c92c5201508b4afc1d04b7eb28
kumarsaurabh20 <kumarsaurabh20@gmail.com>
2013-10-20 01:08:51 +0200

    update

--- lib/convert.rb
+++ lib/convert.rb
@@ -4,6 +4,7 @@ class Convert
 
  def file_read
  
+    
     # variables
     version = "agp-version	2.0"
     organism = "Halochloris"
@@ -19,15 +20,16 @@ class Convert
     object_beg = "" #starting corrdinates of the component/gap on the object in column one
     object_end = "" #ending corrdinates of the component/gap on the object in column one
     part_number = "" #line count for the component/gaps that make up the object in column one
-    component_type = "" #Sequencing status of the component[A=Active finishing, D=draft HTG, F=finished HTG]
+    component_type_contig = "D" #Sequencing status of the component[A=Active finishing, D=draft HTG, F=finished HTG]
                         #[G=whole genome finishing, N=gap with specified size, O=other sequence, P=predraft]
                         #[U=gap of unknown size, W=WGS contig]
+    component_type_gap = "N"
     component_id = "" #if comp_type is !=N, this is a unique identifier for the sequence component
                       #contributing in the object in col one. 
     gap_length = "" #If comp_type is = N. The length of gap
-    component_beg = "" #if col_type != N, represents beg of part of the component seq that contributes to the object
+
                           
-    gap_type = "" #if col_type=N The combination of gap type and linkage indicates weather the gap is captured or not
+    gap_type = "fragment" #if col_type=N The combination of gap type and linkage indicates weather the gap is captured or not
                   #In some cases gaps types are assigned biological values
                   #fragment:gap btn two contigs, clone:gap btn two clone that do not overlap, contig:gap btn clone 
                   #cotig also called "layout gap", centromere:a gap inserted for centromere, short_arm:gap 
@@ -35,9 +37,9 @@ class Convert
                   #Hetrochromatin:a gap inserted for an especially large region of heterochromatic sequence(may also)
                   #include centromere. Telomere:a gap inserted for telomere, repeat:an unresolve repeat 
 
-     component_end = "" #if col_type!=N end part of component contributing in object in col one
 
-     linkage = "" #if col_type=N, indicates if there is evidence of linkage betn the adjacent lines values:yes or No
+
+     linkage = "No" #if col_type=N, indicates if there is evidence of linkage betn the adjacent lines values:yes or No
 
      orientation = "" #if col_type!= N, specifies orientation of component relative to object on col one +/-
 
@@ -65,9 +67,11 @@ class Convert
          posn = []
          gap_end = []
          gap_diff = []
+         component_beg = "1" #if col_type != N, represents beg of part of the component seq that contributes to the object
+        component_end = []  #if col_type!=N end part of component contributing in object in col one
    
       gaps = q.scan(/nnnnnnnnnn+/)  #creates array of string that matches the pattern
-      q.scan(/nnnnnnnnnnn+/) do |gap|
+      q.scan(/nnnnnnnnnn+/) do |gap|
          posn << q.index(gap)
 	 gap_end << "#{q.index(gap)}".to_i + "#{gap}".length.to_i - 1
          q = q.sub(gap, "N" * gap.length)
@@ -98,19 +102,27 @@ class Convert
           n = 0    
           for i in 0..contigs.length - 1
 
-          gap_diff << "#{gap_end[i]}".to_i - "#{posn[i]}".to_i  + 1        
+          gap_diff << "#{gap_end[i]}".to_i - "#{posn[i]}".to_i  + 1 
+          component_end = "#{contigs[i]}".length.to_i + 1       
           
           n = n + 1          
  
-	  file.puts "#{[scaffold_id,contig_beg[i],contig_end[i], n,contig_id[i]].join("\t")}" 
+	  file.puts "#{[scaffold_id,contig_beg[i],contig_end[i], n, component_type_contig, contig_id[i], component_beg, component_end ].join("\t")}" 
           
-          file.puts "#{[scaffold_id,posn[i],gap_end[i], n+1, gap_diff[i]].join("\t")}" 
+          file.puts "#{[scaffold_id,posn[i],gap_end[i], n+1, component_type_gap, gap_diff[i], gap_type, linkage].join("\t")}" unless contigs[i] == contigs.last
                     
           n = n + 1
    
           end
 	      
       end
+
+     #last_line = 0
+     #file = File.open("test.agp", 'r+')
+     #file.each {  last_line = file.pos unless file.eof? }
+     #file.seek(last_line, IO::SEEK_SET)
+     #file.print " "
+     #file.close
  
       #puts contig_id
       #puts q
